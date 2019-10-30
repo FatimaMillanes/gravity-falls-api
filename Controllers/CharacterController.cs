653ecfd33f56d01edab06d09b25d9d9d262d2a20
fatim <fatima.rosela@hotmail.com>
2019-10-30 09:56:25 -0700

    second commit

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using gravity_falls_api.Modules;
 using gravity_falls_api.Dependencies;
 using Microsoft.AspNetCore.Cors;
+using System.Data.SqlClient;
 
 namespace gravity_falls_api.Controllers
 {
@@ -42,8 +43,10 @@ namespace gravity_falls_api.Controllers
                 Age = 15
             },
 
-            
         };
+
+         string connectionString = @"data source=DESKTOP-BBQP2VL; initial catalog=db_gravityfalls; user id=gravityfalls; password=1234";
+        
         [HttpGet("{id}")]
         public Character GetCharacter(int id)
         {
@@ -53,7 +56,28 @@ namespace gravity_falls_api.Controllers
         [HttpGet]
         public List<Character> GetCharacterList()
         {
-            return listOfCharacters;
+            List<Character> characters = new List<Character>();
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("select * from tbl_characters", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Character character = new Character
+                {/*for database*/
+                    Id = reader.GetInt64(reader.GetOrdinal("id")),
+                    FirstName = reader.GetString(reader.GetOrdinal("firstName")),
+                    SecondName = reader.GetString(reader.GetOrdinal("secondName")),
+                    LastName = reader.GetString(reader.GetOrdinal("lastName")),
+                    Description = reader.GetString(reader.GetOrdinal("descp")),
+                    Age=reader.GetInt32(reader.GetOrdinal("age")) 
+                    
+                };
+                characters.Add(character);
+            }
+            conn.Close();
+            return characters;
         }
     }
 }
